87f5c1c5e6f858a3fa744c0a61324bf4b07b6367
Melinda Golden <info@melindagolden.com>
2020-03-25 12:15:38 -0400

    update: Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby TypeScript Starter`,
+    description: `This is a barebones Gatsby starter with TypeScript, ESLint, and Prettier.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -18,7 +19,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-typescript-starter`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
@@ -31,4 +32,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
